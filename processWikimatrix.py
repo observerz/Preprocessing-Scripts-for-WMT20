833c15eaadcbdcf763d2a1e5a12b494504f916b8
observerz <63782098+observerz@users.noreply.github.com>
2023-03-26 22:59:30 +0800

    Update processWikimatrix.py

--- processWikimatrix.py
+++ processWikimatrix.py
@@ -1,18 +1,16 @@
-
-
 enSet = set()
 deSet = set()
-file_write_en = open('/data1/mzlv/csaData/WikiMatrix.en.txt','w')
-file_write_de = open('/data1/mzlv/csaData/WikiMatrix.de.txt','w')
-for line in open('/data1/mzlv/csaData/WikiMatrix.v1.de-en.tsv'):
+file_write_en = open('/data1/mzlv/csaData/processComplete/Original_train_datas/WikiMatrix.en.txt','w')
+file_write_de = open('/data1/mzlv/csaData/processComplete/Original_train_datas/WikiMatrix.de.txt','w')
+for line in open('/data1/mzlv/csaData/DownloadData/WikiMatrix.v1.de-en.tsv'):
     #print(line)
     #de自带\n，en需要加上\n
     temp = line.split('\t')
     
     de,en = temp[1],temp[2]
-    if len(en) > 80 or len(de) > 80 or len(en) == 0 or len(de) == 0:
+    if len(en.split(' ')) > 80 or len(de.split(' ')) > 80:
         continue
-    if en in enSet or de in deSet:
+    if en in enSet and de in deSet:
         continue
     enSet.add(en)
     deSet.add(de)
